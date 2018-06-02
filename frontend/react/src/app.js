4166554265dcd4f57a7a41a543f4e05b5bee7064
ahuang <mengling915@163.com>
2018-06-02 15:06:32 +0800

    added: babel-polyfill test

--- frontend/react/src/app.js
+++ frontend/react/src/app.js
@@ -10,6 +10,8 @@ import Login from '@/containers/login.jsx';
 import createStore from '@/store';
 import '@/scss/index.scss';
 
+import '@/test/babelTest.js';
+
 const store = createStore();
 
 console.log(`---process.env.NODE_ENV ${process.env.NODE_ENV} ---`);
@@ -17,16 +19,6 @@ if (process.env.NODE_ENV === 'development') {
     require('./mock/movie.js');
     require('./mock/user.js');
 }
-
-//  箭头函数语法需要babel-preset-env转译
-const example = {
-    normal: function () { console.log('normal this: ', this); },
-    arrow: () => { console.log('arrow this: ', this); },
-    shorthand() { console.log('shorthand this: ', this); }
-};
-new example.normal();
-new example.arrow();
-new example.shorthand();
 
 
 //  JSX语法需要babel-preset-react转译
