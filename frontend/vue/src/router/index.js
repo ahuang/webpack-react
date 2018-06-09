ef991079d7f5183bbe672ee2d104a1ef4e0dd1d8
ahuang <mengling915@163.com>
2018-06-09 17:05:54 +0800

    added: vue dynamic import

--- frontend/vue/src/router/index.js
+++ frontend/vue/src/router/index.js
@@ -7,69 +7,42 @@ import Login from '@/containers/login.vue';
 import Test from '@/containers/test/index.vue';
 
 Vue.use(VueRouter)
-// const router = new VueRouter({
-//     routes: [{
-//         path: '/',
-//         component: Home
-//     }, {
-//         path: '/news',
-//         component: News
-//     }, {
-//         path: '/music',
-//         component: Music
-//     }, {
-//         path: '/test',
-//         component: Test
-//     }, {
-//         path: '/login',
-//         component: Login
-//     }]
-// });
 
+// 下面2行代码，没有指定webpackChunkName，每个组件打包成一个js文件。
+const ImportFuncDemo1 = () => import('../containers/test/index.vue');
+const ImportFuncDemo2 = () => import('../containers/login.vue')
 
-// const router = new VueRouter({
-//     routes: [{
-//         path: '/',
-//         component: () => import('@/containers/home.vue')
-//     }, {
-//         path: '/news',
-//         component: () => import('@/containers/news.vue')
-//     }, {
-//         path: '/music',
-//         component: () => import('@/containers/music.vue')
-//     }, {
-//         path: '/test',
-//         component: () => import('@/containers/test/index.vue')
-//     }, {
-//         path: '/login',
-//         component: () => import('@/containers/login.vue')
-//     }]
-// });
+// 下面2行代码，指定了相同的webpackChunkName，会合并打包成一个js文件。
+// const ImportFuncDemo1 = () => import(/* webpackChunkName: 'ImportFuncDemo' */ '../containers/test/index')
+// const ImportFuncDemo2 = () => import(/* webpackChunkName: 'ImportFuncDemo' */ '../containers/login')
 
 const router = new VueRouter({
-    mode: 'history',
     routes: [{
         path: '/',
         name: 'home',
-        component: resolve => require(['../containers/home.vue'], resolve)
+        // 按需加载实现1: 使用vue的[异步组件]技术, 一个组件生成一个js文件
+        component: resolve => require(['../containers/home.vue'], resolve),
     }, {
         path: '/news',
         name: 'news',
-        component: () => import('../containers/news.vue')
+        // 按需加载实现2: 使用webpack的[require.ensure]技术
+        // 最后一个参数是指定生成的js文件，如果没有指定，每个组件生成一个js文件
+        component: resolve => require.ensure([], () => resolve(require('../containers/news.vue')), 'tiny')
         
     }, {
         path: '/music',
         name: 'music',
-        component: r => require.ensure([], () => r(require('../containers/music.vue')), 'demo')
+        component: resolve => require.ensure([], () => resolve(require('../containers/music.vue')), 'tiny')
     }, {
         path: '/test',
         name: 'test',
-        component: resolve => require(['../containers/test/index.vue'], resolve)
+        // 按需加载实现3: 使用webpack的[require.ensure]技术
+        component: ImportFuncDemo1
         
     }, {
         path: '/login',
         name: 'login',
-        component: resolve => require(['../containers/login.vue'], resolve)        
+        component: ImportFuncDemo2
     }]
 });
 
